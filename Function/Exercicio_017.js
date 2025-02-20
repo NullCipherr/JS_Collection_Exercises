b6830373c508ff9a9932d32098a90fad9691641f
NullCipherr <blendsteal@gmail.com>
2025-02-20 14:09:34 -0300

    fix exercise 17.

--- Function/Exercicio_017.js
+++ Function/Exercicio_017.js
@@ -5,10 +5,10 @@
 // Crie uma arrow function chamada verificarLevelUp que recebe experienciaAtual e experienciaNecessaria para o próximo nível. 
 // Retorne true se o jogador tiver experiência suficiente para subir de nível, caso contrário, false.
 
-const veriricarLevelUp = (experienciaAtual, experienciaNecessaria) => experienciaAtual >= experienciaNecessaria;
+const verificarLevelUp = (experienciaAtual, experienciaNecessaria) => experienciaAtual >= experienciaNecessaria;
 
 // Testes da função
-console.log(veriricarLevelUp(100, 250)); // false
-console.log(veriricarLevelUp(250, 250)); // true
-console.log(veriricarLevelUp(300, 250)); // true
-console.log(veriricarLevelUp(250, 300)); // false
+console.log(verificarLevelUp(100, 250)); // false
+console.log(verificarLevelUp(250, 250)); // true
+console.log(verificarLevelUp(300, 250)); // true
+console.log(verificarLevelUp(250, 300)); // false
